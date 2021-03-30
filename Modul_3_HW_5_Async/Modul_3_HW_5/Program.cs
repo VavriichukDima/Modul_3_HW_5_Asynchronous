a6cc56e9ffa116045ca1ed517fec39cb8240e9cd
Dima Vavriychuk <tiponchik@gmail.com>
2021-03-30 22:18:54 +0300

    2nd version

--- Modul_3_HW_5_Async/Modul_3_HW_5/Program.cs
+++ Modul_3_HW_5_Async/Modul_3_HW_5/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.IO;
 using System.Threading.Tasks;
 
@@ -35,6 +35,41 @@ namespace Modul_3_HW_5
             Task.WhenAll().GetAwaiter().GetResult();
             var result = ConcatStringAsync();
             Console.WriteLine(result);
+        }
+    }
+} */
+
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Modul_3_HW_5
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            var readFile = ReadFileAsync();
+            var returnString = ReturnStringAsync();
+            var result = string.Concat(readFile, returnString);
+            Console.WriteLine(result);
+
+            static async Task<string> ReadFileAsync()
+            {
+                using (FileStream fstream = File.OpenRead("text.txt"))
+                {
+                    byte[] array = new byte[fstream.Length];
+                    await fstream.ReadAsync(array, 0, array.Length);
+
+                    string textFromFile = System.Text.Encoding.UTF8.GetString(array);
+                    return textFromFile;
+                }
+            }
+
+            static async Task<string> ReturnStringAsync()
+            {
+                return await Task.FromResult("world");
+            }
         }
     }
 }
