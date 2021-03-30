2445c85955b6616f112767ddb1993c403b9dd4a9
VavriichukDima <78602077+VavriichukDima@users.noreply.github.com>
2021-03-30 20:45:55 +0300

    All_solution

---
+++ Modul_3_HW_5_Async/Modul_3_HW_5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Modul_3_HW_5
+{
+    public class Program
+    {
+        private static string ReadFile()
+        {
+            StreamReader sr = new StreamReader("text.txt");
+            return sr.ReadLine();
+        }
+
+        private static async Task<string> ReadFileAsync()
+        {
+            return await Task.FromResult(ReadFile());
+        }
+
+        private static async Task<string> ReturnStringAsync()
+        {
+            return await Task.FromResult("world");
+        }
+
+        private static string ConcatStringAsync()
+        {
+            var readFile = ReadFileAsync();
+            var returnString = ReturnStringAsync();
+            string s = string.Concat(readFile, returnString);
+            return s;
+        }
+
+        private static void Main(string[] args)
+        {
+            Task.WhenAll().GetAwaiter().GetResult();
+            var result = ConcatStringAsync();
+            Console.WriteLine(result);
+        }
+    }
+}
